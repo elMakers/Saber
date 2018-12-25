739681edfb56ac157a2dd44ff68a4566410e85ff
Nathan Wolf <nathan@elmakers.com>
2018-12-25 12:19:14 -0600

    Add swing sounds, WIP

---
+++ Assets/Scripts/Saber.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+	public AudioClip[] SwingSounds;
+	public AudioSource SwingSource;
+	public GameObject TipLocation;
+	public float MinimumSwingSpeed = 0.2f;
+	public float SwingSoundTimeout = 1.0f;
+	
+	private Rigidbody _body;
+	
+	// State
+	private Vector3 _lastLocation;
+	private float _lastSwingSound;
+
+	// Use this for initialization
+	void Start () {
+		_body = TipLocation.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (_lastLocation == Vector3.zero)
+		{
+			_lastLocation = _body.position;
+			return;
+		}
+
+		Vector3 velocity = _body.position - _lastLocation;
+		_lastLocation = _body.position;
+		var currentTime = Time.time;
+		var speed = velocity.magnitude;
+		// currentTime > _lastSwingSound + SwingSoundTimeout
+		if (speed >= MinimumSwingSpeed)
+		{
+			// Debug.Log("SWING? " + currentTime + " : " + (_lastSwingSound + SwingSoundTimeout) + ", speed: " + speed);
+			if (currentTime < _lastSwingSound + SwingSoundTimeout) return;
+			_lastSwingSound = currentTime;
+			var clipIndex = Random.Range(0, SwingSounds.Length);
+			SwingSource.PlayOneShot(SwingSounds[clipIndex]);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Saber
-Saber practice
+
+Saber practice, just for fun!
+
+## Credits
+
+Lightsaber model originally from: https://sketchfab.com/models/50c60595599c4a739b2c959d645bea2d#download
