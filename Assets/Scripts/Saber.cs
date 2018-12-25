1a60b43844441c2ac8c147fa6fae0dc1bfda8c91
Nathan Wolf <nathan@elmakers.com>
2018-12-25 16:15:00 -0600

    Improved swing sounds

--- Assets/Scripts/Saber.cs
+++ Assets/Scripts/Saber.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Saber : MonoBehaviour
 {
@@ -7,20 +9,27 @@ public class Saber : MonoBehaviour
 	public GameObject TipLocation;
 	public float MinimumSwingSpeed = 0.2f;
 	public float SwingSoundTimeout = 1.0f;
+	public int SpeedSamples = 30;
 	
 	private Rigidbody _body;
 	
 	// State
 	private Vector3 _lastLocation;
 	private float _lastSwingSound;
+	private float[] _speedSamples;
+	private int _speedIndex;
+	private int _sampleCount;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		_body = TipLocation.GetComponent<Rigidbody>();
+		_speedSamples = new float[SpeedSamples];
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 		if (_lastLocation == Vector3.zero)
 		{
@@ -32,10 +41,23 @@ public class Saber : MonoBehaviour
 		_lastLocation = _body.position;
 		var currentTime = Time.time;
 		var speed = velocity.magnitude;
-		// currentTime > _lastSwingSound + SwingSoundTimeout
-		if (speed >= MinimumSwingSpeed)
+
+		_speedSamples[_speedIndex] = speed;
+		_sampleCount++;
+		_speedIndex = (_speedIndex + 1) % SpeedSamples;
+		var speedTotal = 0.0f;
+		var speedCount = Math.Min(_sampleCount, SpeedSamples);
+		for (var i = 0; i < speedCount; i++)
 		{
-			// Debug.Log("SWING? " + currentTime + " : " + (_lastSwingSound + SwingSoundTimeout) + ", speed: " + speed);
+			speedTotal += _speedSamples[i];
+		}
+
+		var averageSpeed = speedTotal / speedCount;
+		//Debug.Log("Speed: " + speed + ", avg: " + averageSpeed + " from " + _body.position);
+			
+		if (averageSpeed >= MinimumSwingSpeed)
+		{
+			// Debug.Log("SWING? " + currentTime + " : " + (_lastSwingSound + SwingSoundTimeout) + ", speed: " + averageSpeed);
 			if (currentTime < _lastSwingSound + SwingSoundTimeout) return;
 			_lastSwingSound = currentTime;
 			var clipIndex = Random.Range(0, SwingSounds.Length);
