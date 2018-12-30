f22aea299c440946dea96e726b04f84ee316acc7
Nathan Wolf <nathan@elmakers.com>
2018-12-29 20:00:33 -0600

    W.I.P. Slicing Can't quite figure out how to handle collisions without using velocity stashing for now to implement touchpad suggestion.

---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Slice : MonoBehaviour {
+	// Properties
+	public GameObject Sparks;
+	public Extend Extend;
+	
+	void Start () 
+	{
+	}
+	
+	void Update ()
+	{
+		if (!Extend.Extended()) return;
+		
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		Debug.Log("Triggered");
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collided");
+		if (!Extend.Extended()) return;
+		
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+	}
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG
+
+## 0.2
+
+ - Add world mesh
+ - Effects on hitting the world
+
+## 0.1
+
+ - First release
+ - Trigger to extend/retract
+ - Hum and swing sounds
