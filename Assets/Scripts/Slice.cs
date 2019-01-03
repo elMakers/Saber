bc6b9b15a601a47a73cb1ee6a640e4262f824f80
Nathan Wolf <nathan@elmakers.com>
2019-01-03 07:18:05 -0800

    Automatically expire particles when queue gets full

--- Assets/Scripts/Slice.cs
+++ Assets/Scripts/Slice.cs
@@ -24,11 +24,14 @@ public class Slice : MonoBehaviour {
 	private int _particleQueueIndex;
 	private float _lastSound;
 	private float _sliceSoundCooldown;
+	private ParticleSystem.Particle[] _particles;
+	private int _particlePurgeIndex;
 	
 	void Start()
 	{
 		_extend = GetComponent<Extend>();
 		_particleQueue = new GameObject[ParticleQueueSize];
+		_particles = new ParticleSystem.Particle[InnerDecalParticleSystem.main.maxParticles];
 		ResetSliceCooldown();
 	}
 
@@ -47,24 +50,43 @@ public class Slice : MonoBehaviour {
 		
 		// Raycast to check for surfaces to slice
 		var hits = Physics.RaycastAll(position, direction, Length);
+		if (hits.Length == 0) return;
+
+		var firstHit = hits[0];
+		// TODO: Check for distance travelled
+		
+		// Check for particle limit
+		var count = InnerDecalParticleSystem.GetParticles(_particles);
+		if (count == InnerDecalParticleSystem.main.maxParticles) {
+			Debug.Log("Purging " + _particlePurgeIndex + "[" + _particles[_particlePurgeIndex].remainingLifetime + "]");
+			_particles[_particlePurgeIndex].remainingLifetime = -1;
+			InnerDecalParticleSystem.SetParticles(_particles, count);
+			
+			count = OuterDecalParticleSystem.GetParticles(_particles);
+			if (count == OuterDecalParticleSystem.main.maxParticles)
+			{
+				_particles[_particlePurgeIndex].remainingLifetime = -1;
+				OuterDecalParticleSystem.SetParticles(_particles, count);
+			}
+
+			_particlePurgeIndex = (_particlePurgeIndex + 1) % count;
+		}		
 		// Debug.DrawLine(position, position + direction * Length, Color.yellow, 2.0f, false);
 		
 		// If the middle hit, check surface of blade to see if we are at an edge
 		var adjacentHitCount = 0;
-		if (hits.Length > 0)
+		var distance = (firstHit.point - position).magnitude + Width;
+		// TODO: Don't use distance, check for same object instead
+		for (var i = 0; i < AdjacentCount; i++)
 		{
-			var distance = (hits[0].point - position).magnitude + Width;
-			for (var i = 0; i < AdjacentCount; i++)
+			var angle = new Vector3(Mathf.Cos(Mathf.PI * i / AdjacentCount), Mathf.Sin(Mathf.PI * i / AdjacentCount), 0);
+			var tangent = Vector3.Cross(direction, angle);
+			var adjacentPosition = position + (tangent * Width);
+			if (Physics.Raycast(adjacentPosition, direction, distance))
 			{
-				var angle = new Vector3(Mathf.Cos(Mathf.PI * i / AdjacentCount), Mathf.Sin(Mathf.PI * i / AdjacentCount), 0);
-				var tangent = Vector3.Cross(direction, angle);
-				var adjacentPosition = position + (tangent * Width);
-				if (Physics.Raycast(adjacentPosition, direction, distance))
-				{
-					adjacentHitCount++;
-				}
-				// Debug.DrawLine(adjacentPosition, adjacentPosition + direction * distance, Color.yellow, 2.0f, false);
+				adjacentHitCount++;
 			}
+			// Debug.DrawLine(adjacentPosition, adjacentPosition + direction * distance, Color.yellow, 2.0f, false);
 		}
 		
 		foreach (var hit in hits)
